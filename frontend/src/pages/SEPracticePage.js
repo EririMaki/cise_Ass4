248840d9c87f9578bc898dbed279428a59134757
EririMaki <88325284+EririMaki@users.noreply.github.com>
2022-04-04 23:10:01 +1200

    Frontend problem fixed

---
+++ frontend/src/pages/SEPracticePage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import articles from "../dummydata/articles";
+import Styles from "../components/tablestyle";
+import Table from "../components/evidencetable";
+import tablecolumns from "../components/tablecolumns";
+import Dropdown from "../components/Dropdown";
+
+const SEPracticePage = () => {
+    return (
+      <div>
+        <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+        <Dropdown/>
+        <Styles>
+          <Table
+          data={articles}
+          columns={tablecolumns}
+          />
+        </Styles>
+      </div>
+    );
+}
+ 
+export default SEPracticePage;
+
